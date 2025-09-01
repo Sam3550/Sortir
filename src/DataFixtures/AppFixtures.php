2de5429732be7e756328c2c41abe2a813bcfdf65
Sam3550 <Samir3550.sg@gmail.com>
2025-09-01 16:56:47 +0200

    file add

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,31 +2,134 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Campus;
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
 use App\Entity\Serie;
 use App\Entity\Sortie;
+use App\Entity\Ville;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+
+    {
+
+        $this->passwordHasher = $passwordHasher;
+
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $this->addData($manager);
+    }
+
+    public function addData(ObjectManager $manager)
+    {
+
 
         $faker = Factory::create('fr_FR');
 
-        for ($i = 0; $i < 20; $i++) {
 
+        //Etat
+        $etat = new Etat();
+        $etat
+            ->setLibelle($faker->randomElement(['Créée', 'Ouverte', 'Clôturée', 'Activité en cours', 'Activité terminée', 'Activité archivée', 'Annulée']));
+        $manager->persist($etat);
+        $etats[] = $etat;
+
+        //Ville
+        for ($i = 0;
+             $i < 20;
+             $i++) {
+            $ville = new Ville();
+            $ville
+                ->setNom($faker->realText(20))
+                ->setCodePostal($faker->postcode());
+            $manager->persist($ville);
+            $villes[] = $ville;
+        }
+
+        //Lieu
+        for ($i = 0; $i < 20; $i++) {
+            $lieu = new Lieu();
+            $lieu
+                ->setNom($faker->realText(25))
+                ->setRue($faker->realText(100))
+                ->setLatitude($faker->randomFloat())
+                ->setLongitude($faker->randomFloat());
+
+            $lieu->setVille($faker->randomElement($villes));
+            $lieux[] = $lieu;
+            $manager->persist($lieu);
+
+        }
+
+        //Campus
+        for ($i = 0; $i < 20; $i++) {
+            $campus = new Campus();
+            $campus
+                ->setNom($faker->realText(20));
+            $campusList[] = $campus;
+            $manager->persist($campus);
+
+        }
+
+
+
+        //Participant
+        for ($i = 0; $i < 20; $i++) {
+            $participant = new Participant();
+
+            $thePassword = $faker->password();
+            $hashedPassword = $this->passwordHasher->hashPassword($participant, $thePassword);
+
+            $participant
+                ->setNom($faker->lastName())
+                ->setPrenom($faker->firstName())
+                ->setTelephone($faker->phoneNumber())
+                ->setMail($faker->companyEmail())
+                ->setMotPasse($hashedPassword)
+                ->setActif($faker->boolean(true))
+                ->setOrganisateur($faker->boolean(true));
+            $participant->setCampus($faker->randomElement($campusList));
+            $participants[] = $participant;
+            $manager->persist($participant);
+
+        }
+
+
+        //Sortie
+        for ($i = 0; $i < 20; $i++) {
             $sortie = new Sortie();
             $sortie
-                ->setBackdrop($faker->realText(10))
-                ->setDateCreated(new \DateTime())
-                ->setGenres($faker->randomElement(['Western', 'SF', 'Drama', 'Comedy']))
-                ->setName($faker->realText(10))
-                ->setNbLike($faker->numberBetween(0, 500))
-                ->setFirstAirDate($faker->dateTimeBetween('-6 year'));
+                ->setNom($faker->realText(10))
+                ->setDateHeureDebut(new \DateTime())
+                ->setDuree($faker->realText(20))
+                ->setDateLimiteInscription($faker->date())
+                ->setNbInscriptionMax($faker->numberBetween(15, 20))
+                ->setInfosSortie($faker->realText(500));
+            $sortie
+                ->setCampus($faker->randomElement($campus))
+                ->setEtat($faker->randomElement($etats))
+                ->setLieu($faker->randomElement($lieux))
+                ->addParticipant($faker->randomElement($participants));
+
+
+            $manager->persist($sortie);
+
         }
+
 
         $manager->flush();
     }
+
 }
