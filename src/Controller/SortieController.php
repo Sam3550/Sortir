8c10cf1b91f54097d7c84fafe3d168aa36cc7ae3
sophietande <sophie.tande@gmail.com>
2025-09-02 16:59:45 +0200

    1006 - créer une sortie, gestion du formtype

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\AddSortieFormType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,6 +22,27 @@ final class SortieController extends AbstractController
 
         return $this->render('sortie/afficher.html.twig',[
             'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/addSortie', name: 'addSortie')]
+    public function addSortie(SortieRepository $sortieRepository, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $sortie = new Sortie();
+        $addSortieForm = $this->createForm(AddSortieFormType::class, $sortie);
+
+        $addSortieForm->handleRequest($request);
+
+        if ($addSortieForm->isSubmitted() && $addSortieForm->isValid()) {
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash("success", "Sortie : " . $sortie->getNom() . " ajoutée !");
+            //TODO mettre bon return
+            return $this->redirectToRoute('sortie_addSortie');
+        }
+        return $this->render('sortie/ajouter_sortie.html.twig', [
+            'addSortieForm' => $addSortieForm
         ]);
     }
 }
