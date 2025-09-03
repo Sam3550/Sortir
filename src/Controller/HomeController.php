5d8095beb4bf105a1526e9c286eb10496cc88835
Samuel Moreau <beaj@ik.me>
2025-09-03 14:07:25 +0200

    Connection utilisateur, Se souvenir de moi et Gérer son profil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,33 +2,40 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController extends AbstractController
+class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'home')]
-    public function home(Request $request): Response
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
     {
-        if ($request->isMethod('POST')) {
-            $email = $request->request->get('email');
-            // TODO: Store the email address
-            // For now, we'll just add a flash message
-            $this->addFlash('success', 'Merci ! Nous vous tiendrons au courant.');
-            return $this->redirectToRoute('home');
-        }
-
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
 
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    #[Route('/profile', name: 'app_profile')]
+    public function profile(Request $request, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('home');
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Your profile has been updated.');
+
+            return $this->redirectToRoute('app_profile');
+        }
+
+        return $this->render('home/profile.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
