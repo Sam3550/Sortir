50e141dcb2b760028f368945118ae525b207ef07
Sam3550 <Samir3550.sg@gmail.com>
2025-09-05 15:40:02 +0200

    Alignement des planètes

--- src/Controller/InscritController.php
+++ src/Controller/InscritController.php
@@ -2,33 +2,31 @@
 
 namespace App\Controller;
 
-use App\Entity\Participant;
 use App\Entity\Sortie;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class InscritController extends AbstractController
 {
-
-    #[Route('/inscrit', name: 'inscrit')]
+    #[Route('/inscrit/{id}', name: 'inscrit')]
     public function inscrit(Sortie $sortie, EntityManagerInterface $entityManager): Response
     {
+        /** @var \App\Entity\Participant|null $userConnecter */
         $userConnecter = $this->getUser();
 
         if (!$userConnecter) {
             $this->addFlash('error', 'Il faut que vous soyez connecté pour vous inscrire.');
-            return $this->redirectToRoute('app_login');
-        }else{
-
-
-            $sortie->addParticipant($userConnecter);
-            $entityManager->persist($sortie);
-            $entityManager->flush();
-
+            return $this->redirectToRoute('app_magic_login');
         }
+
+        $sortie->addParticipant($userConnecter);
+        $entityManager->persist($sortie);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Vous êtes bien inscrit à la sortie.');
+
         return $this->redirectToRoute('sortie_list');
     }
 }
